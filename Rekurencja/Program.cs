1cce691752f46541ab38b3ce30bf90ecd7796008
jarek <jarek@LAPTOP-R9P75008>
2022-11-10 17:39:11 +0100

    Dodaj pliki projektów.

---
+++ Rekurencja/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Rekurencja
+{
+    internal class Program
+    {
+        static int RekurencjaSilnia(int liczba)
+        {
+            if(liczba==1)
+                return 1;
+            else
+                return liczba*RekurencjaSilnia(liczba-1);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RekurencjaSilnia(3));
+        }
+    }
+}
